9c2524e8200e51c10c0f31054387ce90e672347b
xecor <xecor2k@hotmail.com>
2013-12-05 16:32:12 +0100

    even more answers

--- questions.py
+++ questions.py
@@ -181,6 +181,103 @@ questions=[{
 "correct":2},
 
 {
+"question":"Hva er en tjener i en klient-/tjenerarkitektur? " ,
+"answer":[
+" En tjener utfører tjenester på vegne av en klient. ",
+" En tjener etterspør tjenester fra klienter. ",
+" En tjener er et program som vil ha noe utført på en annen maskin. ",
+" Ingen av alternativene i a, b og c. "],
+"correct":0},
+
+{
+"question":"En del av et program består av en FOR-løkke, inne i en FOR-løkke, inne i en FOR-løkke. Alle løkkene løper gjennom tallene fra 1 til N. Tidsforbruket for denne programdelen vil være " ,
+"answer":[
+"N^3",
+"3N",
+"3Nlog(N)"],
+"correct":0},
+
+{
+"question":"Hva er hovedoppgaven til en navnetjener?" ,
+"answer":[
+" Å oversette domenenavn til IP-adresser.",
+" Å oversette IP-adresser til domenenavn. ",
+" Å holde rede på definerte variabelnavn når et program kjører. ",
+" Ingen av alternativene i a, b og c."],
+"correct":0},
+
+{
+"question":"Kari har utviklet et programsystem i den bedriften der hun har vært ansatt. Kan hun ta med seg og videreutvikle denne programvaren hvis hun slutter i bedriften? " ,
+"answer":[
+"Ja, i følge Åndsverksloven er det programmereren som har rettighetene til programvaren. ",
+" Nei, det er bedriften som eier programvaren.",
+" Det er uavklart siden dette er ikke regulert av lovgiving. Rettighetene til programvaren må ",
+"avtales mellom partene i hvert enkelt tilfelle. "],
+"correct":1},
+
+{
+"question":"Svart-boks testing er: " ,
+"answer":[
+"Testing der man prøver ut systemets eller modulens indre oppbygging. ",
+"Testing der man ser bort fra systemets eller modulens indre oppbygning og kun tar i ",
+"betraktning de ytre egenskapene. ",
+" Testing der man ser bort fra systemenes eller modulenes indre oppbygning og prøver ut hvordan de ulike delene i et system fungerer sammen. "],
+"correct":1},
+
+{
+"question":"Når kan man ikke benytte seg av binærsøkingsalgoritmen?" ,
+"answer":[
+"Når datamengden er så stor at det vil ta for lang tid. ",
+" Når det er stor sannsynlighet for at det man søker etter ikke finnes i datamengden. ",
+" Når datamengden ikke er sortert. ",
+" Man kan alltid bruke binærsøkingsalgoritmen. "],
+"correct":2},
+
+{
+"question":"Hva er riktig rekkefølge når ulike minnetyper sorteres etter synkende hastighet? " ,
+"answer":[
+"Registre, cache, primærminne, sekundærminne. ",
+" Cache, registre, primærminne, sekundærminne. ",
+" Primærminne, cache, sekundærminne, registre. "],
+"correct":0},
+
+{
+"question":"Hva er i lovverket ikke definer som sensitive personopplysninger: " ,
+"answer":[
+" Etnisk bakgrunn. ",
+" Politisk oppfatning. ",
+" Filosofisk oppfatning. ",
+" Alle alternativene i a, b og c er sensitive personopplysninger. "],
+"correct":3},
+
+{
+"question":"Vi skal kode tegnene a-å, A-Å, 0-9, mellomrom, spørsmålstegn, utropstegn, punktum, kolon, semikolon og bindestrek. Hvor mange binære siffer (engelsk: bits) er nødvendig i kodene når alle tegn skal representeres med like mange binære siffer? " ,
+"answer":[
+"6",
+"7",
+"8"],
+"correct":1},
+
+
+ {
+"question":"Hva blir resultatet når 81 kodes binært (i 2-tallsystemet)? " ,
+"answer":[
+" 1011011 ",
+" 1010101 ",
+" 1010001 ",
+" Ingen av alternativene i a, b og c er riktige. "],
+"correct":2},
+
+{
+"question":"11. Hva tilsvarer ABCD i det heksadesimale tallsystemet (16-tallsystemet) i 10-tallsystemet?" ,
+"answer":[
+" 42561 ",
+" 43981 ",
+" 44981 ",
+" Ingen av alternativene i a, b og c er riktige. "],
+"correct":1},
+
+{
 "question":"" ,
 "answer":[
 "",
@@ -188,6 +285,284 @@ questions=[{
 "",
 ""],
 "correct":},
+
+{
+"question":"QoS er forkortelse for: " ,
+"answer":[
+" Quantity of Storage. ",
+" Quality of Servers. ",
+" Quality of Service. ",
+" Ingen av alternativene i a, b og c er riktige. "],
+"correct":2},
+
+{
+"question":"<p>…</p> i en HTML-fil definerer: " ,
+"answer":[
+" Et avsnitt. ",
+" Et adressefelt. ",
+" En hyperlenke. ",
+" Ingen av alternativene i a, b og c. "],
+"correct":0},
+
+{
+"question":"Vi har en sortert liste med 100 000 000 elementer. Ved binærsøking i denne listen, hvor mange sammenligninger må vi i verste fall gjøre? " ,
+"answer":[
+" Omtrent 20. ",
+" Omtrent 27. ",
+" Omtrent 34. "],
+"correct":1},
+
+{
+"question":" A || !(B &&  er usant (engelsk: false) når: " ,
+"answer":[
+" A false, B true, C true ",
+" A true, B true, C false ",
+" A false, B false, C true ",
+"Uttrykket er ikke usant for noen av alternativene i a, b og c. "],
+"correct":0},
+
+{
+"question":"Hva er entropi? " ,
+"answer":[
+"Et mål på informasjonsinnholdet i en melding.",
+" En algoritme for effektiv koding av symboler.",
+"En feiltilstand som gjør at lesehodet på en harddisk ikke klarer å følge sporene nøyaktig og derfor blander data fra flere spor. ",
+" Ingen av alternativene i a, b og c er riktige. "],
+"correct":0},
+
+{
+"question":"En URL (Uniform Resource Locator) har følgende format:" ,
+"answer":[
+" Protokoll, maskin, sti, fil ",
+" Protokoll, maskin, sti ",
+" Domene, sti, fil ",
+" Ingen av alternativene i a, b og c er riktige. "],
+"correct":0},
+
+{
+"question":"Anta at karakterene har følgende fordeling: A (12 %), B (24 %), C (35 %), D (20 %), E (7 %) og F (2 %). Hva er en korrekt Huffmankoding for A-F? " ,
+"answer":[
+" A: 001 B: 10 C: 11 D: 01 E: 0001 F: 0000 ",
+" A: 010 B: 01 C: 10 D: 111 E: 1110 F: 1111 ",
+" A: 1111 B: 11 C: 1 D. 111 E: 11111 F: 111111 ",
+" Ingen av alternativene i a, b og c. "],
+"correct":0},
+
+{
+"question":"En IP-adresse er: " ,
+"answer":[
+"En numerisk adresse som identifiserer en datamaskin eller annen type enhet i et IP-nettverk. ",
+" En mekanisme som gjør det mulig å kontakte en Internett Provider, for eksempel når det oppstår feil i nettet. ",
+" Et felt som identifiserer data i en relasjonsdatabase. ",
+""],
+"correct":0},
+
+{
+"question":"Hva er inspeksjon av programvare? " ,
+"answer":[
+" Kjøring av programmer for å se om programvaren oppfyller kravene til systemet. ",
+"Overvåking av variablenes verdier under programkjøring for å finne årsakene til feil. ",
+" Gjennomgang av programmer uten at de blir kjørt, med tanke på å finne feil og å forvisse seg om at de er riktige."],
+"correct":3},
+
+{
+"question":"Hva kjennetegner komprimeringsalgoritmer som er tapsløs (lossless)?" ,
+"answer":[
+"Den opprinnelige datamengden kan gjenskapes nøyaktig.",
+"Den komprimerte datamengden er like stor som utgangspunktet.",
+"Egner seg spesielt godt for multimediedata som bilder, lyd og video.",
+"Fjerner bare informasjonsinnhold som ikke er viktig for menneskers oppfatning av \
+informasjonsmengden, for eksempel i et bilde."],
+"correct":0},
+
+{
+"question":"Hva sier Nyquist-regelen om samplingsfrekvensen?" ,
+"answer":[
+"Samplingsfrekvensen må være minst halvparten av den høyeste lydfrekvensen.",
+"Samplingsfrekvensen må være minst den samme som den høyeste lydfrekvensen.",
+"Samplingsfrekvensen må være minst dobbelt så rask som den høyeste lydfrekvensen.",
+"Samplingsfrekvensen må alltid være 20KHz."],
+"correct":2},
+
+{
+"question":"Hva er oppgaven til en programteller (program counter):" ,
+"answer":[
+"Den holder rede på antall kodelinjer i et program.",
+"Den inneholder adressen til neste instruksjon.",
+"Den styrer antall iterasjoner i en FOR-løkke.",
+"Ingen av svarene er riktige."],
+"correct":1},
+
+{
+"question":"Hvor mange symboler kan kodes med 10 bit?" ,
+"answer":[
+"10",
+"512",
+"1024",
+"ingen"],
+"correct":2},
+
+{
+"question":"Hvorfor digitalisere nettverk?" ,
+"answer":[
+"Ønske om å kombinere tjenester.",
+"Enklere å utnytte kapasitet bedre med felles nettverk.",
+"Digital koding kan gi bedre feilsjekk og korrigering av feil.",
+"Alle alternativene a-c er riktige."],
+"correct":3},
+
+{
+"question":"Hva er fokuset i programvarevalideringsfasen i systemutvikling?" ,
+"answer":[
+"Beskrive hva systemet skal gjøre.",
+"Designe hvordan systemet skal oppføre seg.",
+"Teste om systemet stemmer med spesifikasjonen og kundekrav.",
+"Ingen av svarene er riktige."],
+"correct":2},
+
+{
+"question":"Hva er et Denial of Service–angrep?" ,
+"answer":[
+"Å sende så mange forespørsler til en tjener (server) at den ikke klarer å utføre oppgavene sine.",
+"Å bryte seg inn på en tjener (server) og sørge for at den nekter å utføre tjenestene sine.",
+"Å nekte å motta meldinger fra en tjener (server) som da blir opptatt med å sende forespørslene på nytt og på nytt.",
+"Å sende en falsk e-post om problemer med en tjeneste, som for eksempel en nettbank, og lure brukeren\
+ til å avsløre påloggingsinformasjon for å få løst problemet."],
+"correct":0},
+
+{
+"question":"Hva vil det si at vi at vi har “random access” (tilfeldig tilgang) til minnet?" ,
+"answer":[
+"All data i minnet kan hentes direkte uansett hvor det befinner seg.",
+"Det er tilfeldig hva som hentes ut av minnet.",
+"Vi må hente ut data sekvensielt (byte for byte) for å finne det vi leter etter.",
+"Ingen av svarene er riktige."],
+"correct":0},
+
+{
+"question":"Hva vil det si at en datamaskin er deterministisk?" ,
+"answer":[
+"Den har en pessimistisk livsanskuelse som avviser fri vilje.",
+"Når den skal velge hvilken instruksjon den skal behandle neste gang har den ikke\
+ noe valg, men baserer valget på programmet og dataene den gis.",
+"At den har en intuisjon på hva som er lurt å gjøre.",
+"Ingen av svarene er riktige."],
+"correct":1},
+
+{
+"question":"Hvilke av alternativene under er mulige tolkninger av PandA-mønstre?" ,
+"answer":[
+"True og False.",
+"Ja og Nei.",
+"+ og –.",
+"Alle alternativene a-c er riktige."],
+"correct":3},
+
+{
+"question":"Vi deler inn algoritmer i klasser basert på de funksjoner som beskriver deres\
+ utvikling i kjøretid best. Hvilken algoritme er i klassen Θ(log(n)):" ,
+"answer":[
+"Innstikksortering (Insertion sort).",
+"Binærsøk (binary search).",
+"Sekvensielt søk (sequential search).",
+"Ingen av svarene er riktige."],
+"correct":1},
+
+{
+"question":"Når vi studerer algoritmers effektivitet, ser vi på hvordan kjøretiden utvikler seg i\
+ forhold til mengden av input. Vi gjør analyser av bestefall, verstefall og\
+ gjennomsnittstilfellet. Hvorfor er det spesielt interessant å analysere en algoritmes\
+ kjøretid i verste fall?" ,
+"answer":[
+"Det setter en øvre grense for hvor lang tid det tar å kjøre algoritmen.",
+"Programmerere er pessimister.",
+"Det er mer interessant med høye tall.",
+"Det forteller hvor lang tid algoritmen ca. bruker på å kjøre."],
+"correct":0},
+
+{
+"question":"Hva er et datagram?" ,
+"answer":[
+"Et telegram som er skrevet på data.",
+"Vekten på en dataenhet.",
+"En pakke som sendes over internett som følger IP-protokollen.",
+"Ingen av svarene er riktige."],
+"correct":2},
+
+{
+"question":"Når vi overfører data over internett oppstår det ofte feil på grunn av forstyrrelser\
+ på linjene. For å oppdage slike feil brukes ofte" ,
+"answer":[
+"NIC (Network Interface Card).",
+"ISP (Internet Service Provider).",
+"CRC (Cyclic Redundancy Check).",
+"Ingen av svarene er riktige."],
+"correct":2},
+
+{
+"question":"Fargene som vises på en dataskjerm representeres ofte med 24 bits RGB-koding.\
+ Fargen blå vil da representeres som:" ,
+"answer":[
+"0000 0000 0000 0000 0000 0000.",
+"1111 1111 0000 0000 0000 0000.",
+"0000 0000 1111 1111 0000 0000.",
+"0000 0000 0000 0000 1111 1111."],
+"correct":3},
+
+{
+"question":"En mikroprosessor utfører de samme fem oppgavene om og om igjen. Hvilken\
+ rekkefølge av stegene under beskriver korrekt rekkefølge på dette F/E-kretsløpet\
+ (F/E cycle)?" ,
+"answer":[
+"Instruction Fetch – Data Fetch – Instruction Decode – Instruction Execution – Results Return.",
+"Results Return – Instruction Execution – Instruction Fetch – Data Fetch – Instruction Decode.",
+"Instruction Fetch – Instruction Decode – Data Fetch – Instruction Execution – Results Return.",
+"Instruction Decode – Instruction Execution – Instruction Fetch – Data Fetch – Results Return."],
+"correct":2},
+
+{
+"question":"Hvilken bestemt endring har vi sett de siste årene innen systemutvikling?" ,
+"answer":[
+"Spesifisering av krav er ikke lengre relevant.",
+"Smidig (agile) systemutvikling har overtatt mer og mer for plandrevet systemutvikling.",
+"Vannfallsmodellen har overtatt for inkrementell systemutvikling.",
+"Ingen av svarene er riktige."],
+"correct":1},
+
+{
+"question":"Hva er DAC?" ,
+"answer":[
+"Et program som hjelper med beregninger (data-assisted computing).",
+"En enhet som oversetter analoge signaler til digitale signaler.",
+"En enhet som oversetter digitale signaler til analoge signaler.",
+"Et program som oversetter datakode til programmeringsspråket C (evt. C++)."],
+"correct":2},
+
+{
+"question":"Rekursjon betyr at" ,
+"answer":[
+"En funksjon kaller seg selv.",
+"Kjøretiden til programmet minsker.",
+"Programmet går i evig løkke.",
+"Ingen av svarene er riktige."],
+"correct":0},
+
+{
+"question":"Ranger effektivitetsklassene Θ(n3), Θ(n), Θ(log(n)) og Θ(n2) etter effektivitet, der\
+ minst effektiv først og deretter mer og mer effektiv" ,
+"answer":[
+"Θ(n3), Θ(n2), Θ(n), Θ(log(n)).",
+"Θ(n3), Θ(n), Θ (n2), Θ(log(n)).",
+"Θ(log(n)),Θ(n3), Θ(n2), Θ(n).",
+"Θ(n), Θ(log(n)),Θ(n3), Θ(n2)."],
+"correct":0},
+
+
+
+
+
+ 
+
 
 
 
