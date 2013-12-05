9beffddc5a77ca913db8f1abae2ad833322c293e
xecor <xecor2k@hotmail.com>
2013-12-05 15:26:00 +0100

    added some questions

--- questions.py
+++ questions.py
@@ -1,29 +1,30 @@
-questions = [
-	{
-		'question': 'Er Christian kul?1',
-		'answers': [
-			'Ja',
-			'Nei(???)',
-			'Ja'
-		],
-		'correct': 0
-	},
-	{
-		'question': 'Er Christian kul?2',
-		'answers': [
-			'Ja',
-			'Nei(???)',
-			'Ja'
-		],
-		'correct': 0
-	},
-	{
-		'question': 'Er Christian kul?3',
-		'answers': [
-			'Ja',
-			'Nei(???)',
-			'Ja'
-		],
-		'correct': 0
-	},
-]
+questions=[{
+"question":"Hva er et hovedkort (motherboard)",
+"answers":[
+"Et kretskort i en datamaskin der enheter som CPU, RAM, og andre enheter kobles sammen.",
+"En minnekrets som tar vare på systemets innstillinger. ",
+"En prosessor for tynne mobiltelefoner. ",
+"Bunnplata i et PC kabinett."],
+"correct":0},
+
+{
+"question":"Hva er en pakke (packet) i nettverkssammenheng?" ,
+"answer":[
+"En datablokk av fast lengde som sendes gjennom nettverket, fra avsender til mottaker. ",
+"En datamelding som har ankommet og som står i kø for å bli levert til mottakermaskinen. ",
+"Den datamengden som utveksles mellom to datamaskiner som kommuniserer via nettverket. ",
+"Ingen av alternativene er riktig."],
+"correct":0}
+
+
+
+
+
+
+
+
+
+
+           ]
+
+
