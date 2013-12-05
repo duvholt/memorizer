ec72949611651fb8119ad99d44697de0626d6459
xecor <xecor2k@hotmail.com>
2013-12-05 15:59:10 +0100

    more answers

--- questions.py
+++ questions.py
@@ -1,5 +1,5 @@
 questions=[{
-"question":"Hva er et hovedkort (motherboard)",
+"question":"Hva er et hovedkort (motherboar",
 "answers":[
 "Et kretskort i en datamaskin der enheter som CPU, RAM, og andre enheter kobles sammen.",
 "En minnekrets som tar vare på systemets innstillinger. ",
@@ -14,13 +14,180 @@ questions=[{
 "En datamelding som har ankommet og som står i kø for å bli levert til mottakermaskinen. ",
 "Den datamengden som utveksles mellom to datamaskiner som kommuniserer via nettverket. ",
 "Ingen av alternativene er riktig."],
-"correct":0}
+"correct":0},
 
 
 
+{
+"question":"Hvilken av disse lagringsenhetene er ikke en sekundærlagrings-enhet? " ,
+"answer":[
+" En harddisk. ",
+" En datamaskins hurtigbuffer (cache). ",
+" En minnepinne.",
+" Alle alternativene er sekundærlagringsenheter. "],
+"correct":1},
 
+{
+"question":" Hva er en protokoll i nettverkssammenheng?" ,
+"answer":[
+"Et sett kommunikasjonsregler for utveksling av data. ",
+" En avtale mellom nettverkseier og en bedrift som bruker nettet. ",
+" Et register der all nettverkstrafikk blir lagret i henhold til kravene fra myndighetene. ",
+"Ingen av alternativene er riktig. "],
+"correct":0},
 
+{
+"question":" Hvilket mål brukes vi vanligvis på overføringskapasitet i nettverk? " ,
+"answer":[
+" Bits pr sekund (bps). ",
+" Gigabyte. ",
+" Båndbredde. ",
+" Ingen av alternativene er riktig. "],
+"correct":0},
 
+{
+"question":"Hva definerer et klient/tjener (client/server) forhold? " ,
+"answer":[
+"Klienter tilbyr data og tjenester til tjenere. ",
+"Klienter og tjenere tilbyr data og tjenester til hverandre. ",
+" Tjenere tilbyr data og tjenester til klienter. ",
+" Ingen av alternativene er riktig. "],
+"correct":2},
+
+{
+"question":"Hva definerer et ”peer-to-peer” nettverk? " ,
+"answer":[
+" En er sjef, de andre er slaver. ",
+" En er slave, de andre er sjefer. ",
+" Alle er likeverdige. ",
+" Ingen av alternativene er riktig. "],
+"correct":2},
+
+{
+"question":"Hvordan kan en GPS bestemme en posisjon? " ,
+"answer":[
+"En GPS beregner sin posisjon ved å lokalisere nærmeste mobile basestasjon. ",
+"En GPS beregner sin posisjon ved å bruke lokasjonen til flere satellitter. ",
+"En GPS beregner sin posisjon ved å bruke lokasjon til kun en satellitt. ",
+" Ingen av alternativene er riktig. "],
+"correct":1},
+
+{
+"question":"Hva er Wi-Fi? " ,
+"answer":[
+"Et sett av standarder for trådløs dataoverføring. ",
+"En kvalitetsbetegnelse for trådløse nett. ",
+" Et mål på kvaliteten på en bredbåndabonnentslinje inn til huset. ",
+"Ingen av alternativene er riktig. "],
+"correct":0},
+
+{
+"question":"En device driver er: " ,
+"answer":[
+"en spesialdatamaskin for kjøretøy. ",
+" spesialisert programvare for input/output, slik at utstyr kan kommunisere med resten av systemet. ",
+"enheten som holder rede på neste instruksjon som skal utføres av en prosessor.",
+" Ingen av alternativene er riktig. "],
+"correct":1},
+
+{
+"question":"Et maskinspråk (machine language) er: " ,
+"answer":[
+"et programmeringsspråk som oversettes av en kompilator (oversetter) til kjørbar kode. ",
+" et binær-type programmeringsspråk bygd inn i prosessoren som datamaskinen kan kjøre direkte. ",
+" er programmeringsspråk som er felles for alle datamaskiner slik at de kan kommunisere. ",
+" Ingen av alternativene er riktig. "],
+"correct":1},
+
+{
+"question":"Ordstørrelse (word size) for en prosessor er: " ,
+"answer":[
+"antall ord i en tekst som kan sammenlignes i et søk. ",
+"antall bokstaver som kan behandles i en tekststreng. ",
+"antall bit en prosessor kan prosessere på en gang. ",
+" Ingen av alternativene er riktig. "],
+"correct":2},
+
+{
+"question":"Ytelse for superdatamaskiner måles i: " ,
+"answer":[
+" FLOPS. ",
+" Gigabytes. ",
+" Antall prosessorkjerner. ",
+" Ingen av alternativene er riktig. "],
+"correct":0},
+
+{
+"question":"Systemklokka i en datamaskin: " ,
+"answer":[
+" fordeler tiden som brukes på ulike programmer. ",
+"bestemmer hvor raskt operasjoner i en mikroprosessor utføres. ",
+" sørger for at dato og tid alltid er riktig satt. ",
+" Ingen av alternativene er riktig. "],
+"correct":1},
+
+{
+"question":"Hovedformålet med forstudiefasen (fase 1) i utvikling av informasjonssystemer er: " ,
+"answer":[
+" Dokumentere krav til systemet. ",
+" Programmere systemet. ",
+" Gjennomføre en forberedende analyse. ",
+" Ingen av alternativene er riktig. "],
+"correct":2},
+
+{
+"question":"Hva vil det si å vedlikeholde et informasjonssystem? " ,
+"answer":[
+"Rette opp eksisterende feil i systemet.",
+" Utføre endringer i systemet basert på nye betingelser. ",
+"Oppdatere dokumentasjon. ",
+" Alle alternativene er riktig. "],
+"correct":3},
+
+{
+"question":"Hva gjør en enhetstest? " ,
+"answer":[
+" Tester at ulike deler av systemet fungerer sammen på korrekt måte. ",
+" Tester at selve datamaskinen (maskinvaren) fungerer. ",
+" Tester individuelle deler av programvaren. ",
+" Ingen av alternativene er riktig. "],
+"correct":2},
+
+{
+"question":"Hva er en algoritme? " ,
+"answer":[
+"Krav som stilles til et dataprogram. ",
+"En test for å finne feil i et dataprogram. ",
+"En presis beskrivelse av operasjoner som skal utføres for å løse et problem. ",
+" Ingen av alternativene er riktig. "],
+"correct":2},
+
+{
+"question":"Hva er et flytskjema? " ,
+"answer":[
+"Grafisk representasjon av en algoritme. ",
+"Et skjema for å fylle inn informasjon på en webside. ",
+" Et skjema som dokumenterer sikkerhet i et databasesystem. ",
+"Ingen av alternativene er riktig. "],
+"correct":0},
+
+{
+"question":"Hva står ACID for innen databaser? " ,
+"answer":[
+"Appropriate, Cynical, Isolation, Development. ",
+" Appropriate, Collaborative, Irrelevant, Driver. ",
+" Atomicity, Consistency, Isolation, Durability. ",
+" Ingen av alternativene er riktig."],
+"correct":2},
+
+{
+"question":"" ,
+"answer":[
+"",
+"",
+"",
+""],
+"correct":},
 
 
 
