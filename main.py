dcf2282075eb8c7a126ee1922b506ef0f1ac9f69
Christian Duvholt <cxhristian@gmail.com>
2014-05-05 21:54:47 +0200

    Added cache busting for static files

--- main.py
+++ main.py
@@ -234,5 +234,21 @@ def utility_processor():
         return 0
     return dict(percentage=percentage)
 
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
 if __name__ == '__main__':
     app.run()
