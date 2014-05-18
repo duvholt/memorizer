3c97579626fc6fab8e29f15ba706442627869325
Christian Duvholt <cxhristian@gmail.com>
2014-05-18 16:33:42 +0200

    Moved importing to a separate file

--- main.py
+++ main.py
@@ -146,51 +146,6 @@ def show_question(course_code, exam_name, id):
     return render_template('question.html', **context)
 
 
-@app.route('/import')
-def import_questions():
-    with app.app_context():
-        # Reset database
-        models.db.drop_all()
-        models.db.create_all()
-    # Load exams from folder with json files
-    for filename in os.listdir('questions'):
-        if os.path.isdir(os.path.join('questions', filename)):
-            # Skip folders
-            continue
-        with open('questions/' + filename, encoding='utf-8') as f:
-            course_json = json.load(f)
-        # Get or create course
-        course = models.Course.query.filter_by(code=course_json['code'], name=course_json['name']).first()
-        if not course:
-            course = models.Course(course_json['code'], course_json['name'])
-            db.session.add(course)
-            db.session.commit()
-        # Get or create exam
-        exam = models.Exam.query.filter_by(name=course_json['exam'], course=course).first()
-        if not exam:
-            exam = models.Exam(course_json['exam'], course.id)
-            db.session.add(exam)
-            db.session.commit()
-        questions = course_json['questions']
-        for question in questions:
-            image = question['image'] if 'image' in question else ""
-            question_object = models.Question(question['question'], exam.id, image)
-            db.session.add(question_object)
-            db.session.commit()
-            for number, answer in enumerate(question['answers']):
-                if type(question['correct']) is int and question['correct'] == number:
-                    correct = True
-                elif type(question['correct']) is list and number in question['correct']:
-                    correct = True
-                else:
-                    correct = False
-                # Setting image if it exists
-                alternative = models.Alternative(answer, number, correct, question_object.id)
-                db.session.add(alternative)
-            db.session.commit()
-    return redirect(url_for('main'))
-
-
 def random_id(id=None, course=None, exam=None):
     """Returns a random id from questions that have not been answered. Returns a complete random number if none available"""
     rand = id
