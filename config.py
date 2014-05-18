1e1869e79ce1c65399b95ea7f3d0cf3ba5ecc5ab
Christian Duvholt <cxhristian@gmail.com>
2014-05-18 17:34:52 +0200

    Improved default config

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+
+PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = False
 SECRET_KEY = 'rebumd8xvrywgkvufsnrtvji7fyjeu4tue'
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(PROJECT_PATH, 'memorizer.db')
 
 try:
     from localconfig import *
