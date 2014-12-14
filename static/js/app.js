6e90b2bf0682fc261c45927fb87ab05e8d92033c
Christian Duvholt <cxhristian@gmail.com>
2014-12-14 01:24:36 +0100

    Rewrite jQuery code to 'normal' JavaScript

--- static/js/app.js
+++ static/js/app.js
@@ -4,43 +4,41 @@
  * - Answer: {spacebar}
  * - Next/Previous question: Arrow right/left
  */
- $('#shortkeys').popover({ 'placement': 'bottom', 'container': 'body', 'trigger': 'hover', 'html': true});
-$(document).keydown(function(e) {
-
-	// The current question
-	var currentQuestion = parseInt(window.location.pathname.split('/')[2]);
-
+document.addEventListener('keydown', function(e) {
 	// Select an alternative
 	try {
-		alternative = e.keyCode - 49;
-		$('[name="answer"]')[alternative].checked = true;
+		var alternative = e.keyCode - 49;
+		document.querySelectorAll('[name="answer"]')[alternative].checked = true;
 	} catch (error) {
 		// Not a valid alternative
 	}
 	
 	// Submit answer to current question
 	if (e.keyCode == 32 || e.keyCode == 81) {
-		$('form:first').submit();
+		document.querySelector('form').submit();
 	}
 	
 	// Previous question
 	if (e.keyCode == 37 || e.keyCode == 65) {
-		if($('#prev_question').size() > 0) {
-			window.location = $('#prev_question').attr('href');
+		var prev = document.getElementById('prev_question');
+		if (prev !== null) {
+			window.location = prev.href;
 		}
 	}
 	
 	// Next question
 	if (e.keyCode == 39 || e.keyCode == 68) {
-		if($('#next_question').size() > 0) {
-			window.location = $('#next_question').attr('href');
+		var next = document.getElementById('next_question');
+		if (next !== null) {
+			window.location = next.href;
 		}
 	}
 	
 	// Random question
 	if (e.keyCode == 38 || e.keyCode == 87) {
-		if ($('#random_question').size() > 0) {
-			window.location = $('#random_question').attr('href');
+		var random = document.getElementById('random_question');
+		if (random !== null) {
+			window.location = random.href;
 		}
 	}
 });
