888766499fda772e4ec5472eb737a5c0895fe7fb
Christian Duvholt <cxhristian@gmail.com>
2015-03-26 15:00:41 +0100

    Add course form and exam page

---
+++ forms.py
@@ -0,0 +1,6 @@
+import models
+from flask_wtf import Form
+from wtforms.ext.sqlalchemy.orm import model_form
+
+CourseForm = model_form(models.Course, Form)
+ExamForm = model_form(models.Exam, Form)
--- views/admin.py
+++ views/admin.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from forms import CourseForm, ExamForm
 import models
 
 
@@ -15,18 +16,17 @@ def courses():
 
 def course(course_code):
     course = models.Course.query.filter_by(code=course_code).first_or_404()
-    context = dict(course=course)
+    form = CourseForm(obj=course)
+    context = dict(course=course, form=form)
     return render_template('admin/course.html', **context)
 
 
-# Exams
-
-def exams():
-    pass
-
-
-def exam(exam_id):
-    pass
+def exam(course_code, exam_name):
+    course = models.Course.query.filter_by(code=course_code).first_or_404()
+    exam = models.Exam.query.filter_by(course=course, name=exam_name).first_or_404()
+    form = ExamForm(obj=exam)
+    context = dict(exam=exam, course=course, form=form)
+    return render_template('admin/exam.html', **context)
 
 
 # Questions
