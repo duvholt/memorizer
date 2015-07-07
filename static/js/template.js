dc29d7abc0754860db850f095e17aeffbaf2f196
Christian Duvholt <cxhristian@gmail.com>
2015-07-07 19:48:04 +0200

    Remove requirement that top level variables have to start with t. from last commit

--- static/js/template.js
+++ static/js/template.js
@@ -36,28 +36,28 @@
         if(s.idMatch.test(str)) {
             return scoop.compile(document.getElementById(str));
         }
-        function_code = 'var code = \'';
+        function_code = 'var $code; with($context) { $code = \'';
         function_code += str.
         replace(
             s.print, function(match, value) {
-                return '\'; code += scoop.encode(' + value + '); code += \'';
+                return '\'; $code += scoop.encode(' + value + '); $code += \'';
             }
         ).replace(
             s.printSafe, function(match, value) {
-                return '\'; code += ' + value + '; code += \'';
+                return '\'; $code += ' + value + '; $code += \'';
             }
         ).replace(
             s.forloop, function(match, element, iterator) {
                 return '\'; for(var i = 0; i < ' + iterator + '.length; i++) { ' +
-                    'var ' + element + ' = ' + iterator + '[i]; code += \'';
+                    'var ' + element + ' = ' + iterator + '[i]; $code += \'';
             }
         ).replace(
             s.endforloop, function(match) {
-                return '\'; } code += \'';
+                return '\'; } $code += \'';
             }
         );
-        function_code += '\'; return code;';
-        return new Function('t', function_code);
+        function_code += '\';} return $code;';
+        return new Function('$context', function_code);
     };
 
 
