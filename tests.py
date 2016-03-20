8a8fadd4561925dfbd870babd4d39be7ac6742a1
Christian Duvholt <cxhristian@gmail.com>
2016-03-20 20:55:56 +0900

    Add database model tests

--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import unittest
 from flask.ext.testing import TestCase
 from flask import Flask
@@ -32,20 +33,112 @@ class ApplicationTest(MemorizerTestCase):
         self.assertIsInstance(app, Flask)
 
 
-class TestUserModel(DatabaseTestCase):
-    def test_add(self):
+class ModelTestCase:
+    model = None
 
-        user = models.User()
-        db.session.add(user)
+    def create(self):
+        return self.model()
+
+    def edit(self, object):
+        raise NotImplementedError
+
+    def test_add(self):
+        object = self.create()
+        db.session.add(object)
         db.session.commit()
 
-        # this works
-        assert user in db.session
+        assert object in db.session
+        self.assertIsNotNone(self.model.query.get(object.id))
 
-        self.client.get("/")
+    def test_edit(self):
+        object = self.create()
+        db.session.add(object)
+        db.session.commit()
 
-        # this raises an AssertionError
-        assert user in db.session
+        object_copy = self.copy_object(object)
+
+        self.assertTrue(self.equal_keys(object, object_copy))
+
+        self.edit(object)
+        db.session.add(object)
+        db.session.commit()
+        self.assertFalse(self.equal_keys(object, object_copy))
+
+    def copy_object(self, object):
+        object_keys = object.__table__.columns.keys()
+        object_keys.remove('id')
+        return {key: getattr(object, key) for key in object_keys}
+
+    def equal_keys(self, object, object2):
+        for key in object2.keys():
+            if getattr(object, key) != object2[key]:
+                return False
+        return True
+
+    def test_delete(self):
+        object = self.create()
+        db.session.add(object)
+        db.session.commit()
+
+        self.assertIsNotNone(self.model.query.get(object.id))
+        db.session.delete(object)
+        db.session.commit()
+        assert object not in db.session
+        self.assertIsNone(self.model.query.get(object.id))
+
+
+class TestUserModel(ModelTestCase, DatabaseTestCase):
+    model = models.User
+
+    def edit(self, user):
+        user.name = "User"
+        user.username = "user"
+
+
+class TestCourseModel(ModelTestCase, DatabaseTestCase):
+    model = models.Course
+
+    def create(self):
+        return self.model(code="TEST", name="Test Course")
+
+    def edit(self, course):
+        course.code = "TEST2"
+        course.name = "Test Course 2"
+
+
+class TestExamModel(ModelTestCase, DatabaseTestCase):
+    model = models.Exam
+
+    def edit(self, exam):
+        exam.name = "Exam"
+
+
+class TestQuestionModel(ModelTestCase, DatabaseTestCase):
+    model = models.Question
+
+    def edit(self, question):
+        question.text = "Text"
+
+
+class TestAlternativeModel(ModelTestCase, DatabaseTestCase):
+    model = models.Alternative
+
+    def edit(self, alternative):
+        alternative.text = "Text"
+        alternative.correct = False
+
+
+class TestStatsModel(ModelTestCase, DatabaseTestCase):
+    model = models.Stats
+
+    def create(self):
+        MyObject = type('MyObject', (object,), {})
+        obj = MyObject()
+        obj.id = -1
+        return self.model(user=obj, question=obj, correct=False)
+
+    def edit(self, stats):
+        stats.correct = True
 
 
 class CacheTest(MemorizerTestCase):
