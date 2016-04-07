8ac8f791a8ae68d2a11685015fb3f14a5d45279a
Christian Duvholt <cxhristian@gmail.com>
2016-04-07 22:53:51 +0200

    Quiz login tests

--- tests/testquiz.py
+++ tests/testquiz.py
@@ -1,6 +1,7 @@
 from flask import url_for
 from unittest.mock import patch
 
+from memorizer.database import db
 from memorizer.models import User
 from tests import DatabaseTestCase
 
@@ -44,4 +45,50 @@ class QuizRegisterTest(DatabaseTestCase):
             'username': username,
             'password': password,
             'confirm': password
+        }
+
+
+class QuizLoginTest(DatabaseTestCase):
+    @patch('memorizer.views.quiz.get_user', return_value=mock_user())
+    def test_login_get(self, mock_get_user):
+        response = self.client.get('/login/')
+        self.assert_200(response)
+
+    @patch('memorizer.views.quiz.get_user', return_value=mock_user(registered=True))
+    def test_login_already_logged_in(self, mock_get_user):
+        response = self.client.get('/login/')
+        self.assert_redirects(response, url_for('quiz.main'))
+
+    @patch('memorizer.views.quiz.get_user', return_value=mock_user())
+    def test_login_successful(self, mock_get_user):
+        username = 'Test1'
+        password = 'password2'
+        user = User()
+        user.username = username
+        user.password = password
+        user.registered = True
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.client.post('/login/', data=self.login_user_data(username, password))
+        self.assert_redirects(response, url_for('quiz.main'))
+
+    @patch('memorizer.views.quiz.get_user', return_value=mock_user())
+    def test_login_fail(self, mock_get_user):
+        username = 'username1'
+        password = 'pass1'
+        user = User()
+        user.username = username
+        user.password = password
+        user.registered = True
+        db.session.add(user)
+        db.session.commit()
+
+        response = self.client.post('/login/', data=self.login_user_data(username, 'pass2'))
+        self.assert_200(response)
+
+    def login_user_data(self, username='Test', password='password1'):
+        return {
+            'username': username,
+            'password': password
         }
