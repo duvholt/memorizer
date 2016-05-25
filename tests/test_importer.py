79f1a546cb802b896a35185a7efb023425242221
Christian Duvholt <cxhristian@gmail.com>
2016-05-25 02:22:01 +0200

    More importer tests

--- tests/test_importer.py
+++ tests/test_importer.py
@@ -1,7 +1,8 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import call, patch
 
-from memorizer import importer
+from memorizer import importer, models
+from tests import DatabaseTestCase
 
 
 class ValidateQuestions(TestCase):
@@ -149,3 +150,82 @@ class ValidateExam(TestCase):
             importer.validate_exam({'code': 'code', 'name': 'Name', 'exam': ''})
 
         importer.validate_exam({'code': 'code', 'name': 'Name', 'exam': 'Exam'})
+
+
+class ImportCommandTest(TestCase):
+    @patch('memorizer.importer.import_exam')
+    @patch('json.load')
+    def test_run(self, json_load_patch, import_exam_patch):
+        import_exam_patch.reset_mock()
+        filenames = ['file1', 'file2', 'file3']
+        command = importer.ImportCommand()
+        command.run(filenames)
+        assert import_exam_patch.call_count == len(filenames)
+        json_load_patch.assert_has_calls([call(f) for f in filenames], any_order=True)
+
+
+class JsonImportTest(DatabaseTestCase):
+    def test_import_exam(self):
+        exam = {
+            "name": "Innføring i medisin for ikke-medisinere",
+            "code": "MFEL1010",
+            "exam": "H10",
+            "questions": [
+                {
+                    "question": "Question 1",
+                    "answers": [
+                        "Alternative 1.1",
+                        "Alternative 1.2",
+                        "Alternative 1.3",
+                        "Alternative 1.4"
+                    ],
+                    "correct": 0
+                },
+                {
+                    "question": "Question 2",
+                    "answer": True
+                },
+                {
+                    "question": "Question 3",
+                    "answers": [
+                        "Alternative 3.1",
+                        "Alternative 3.2",
+                        "Alternative 3.3",
+                        "Alternative 3.4"
+                    ],
+                    "correct": [1, 2]
+                },
+                {
+                    "question": "Question 4",
+                    "answer": False
+                }
+            ]
+        }
+        importer.import_exam(exam)
+        assert models.Question.query.count() == len(exam['questions'])
+        exam_obj = models.Exam.query.first()
+        self.assertEqual(exam_obj.name, exam['exam'])
+        self.assertEqual(exam_obj.course.name, exam['name'])
+        self.assertEqual(exam_obj.course.code, exam['code'])
+        # Check that questions were added to database with correct alternatives
+        for question in exam['questions']:
+            self.validate_question(question)
+
+    def validate_question(self, question):
+        obj = models.Question.query.filter_by(text=question['question']).first()
+        self.assertIsNotNone(obj)
+        if obj.multiple:
+            for alternative in obj.alternatives:
+                self.validate_alternative(question, alternative)
+        else:
+            self.assertEqual(obj.correct, question['answer'])
+
+    def validate_alternative(self, question, alternative):
+        self.assertIn(alternative.text, question['answers'])
+        index = question['answers'].index(alternative.text)
+        correct_answer = question['correct']
+        if type(correct_answer) is list:
+            correct = index in correct_answer
+        else:
+            correct = index == correct_answer
+        self.assertEqual(correct, alternative.correct)
