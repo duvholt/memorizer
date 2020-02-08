5068be632ee15f3eaf74fef15016688e948e3815
Christian Duvholt <christian@duvholt.net>
2020-02-08 20:04:49 +0100

    Configure using environment variables and add example docker compose file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.6-alpine
+FROM python:3.6-slim-buster
 
 RUN pip install pipenv
 
--- memorizer/config.py
+++ memorizer/config.py
@@ -1,23 +1,21 @@
 from datetime import timedelta
 from os import pardir
 from os.path import abspath, dirname, join
+from decouple import config
 
 PROJECT_PATH = abspath(join(dirname(__file__), pardir))
 
-DEBUG = False
-SECRET_KEY = 'rebumd8xvrywgkvufsnrtvji7fyjeu4tue'
+DEBUG = config('MEMORIZER_DEBUG', False)
+SECRET_KEY = config('MEMORIZER_SECRET_KEY', 'super-secret')
 PERMAMENT_SESSION_LIFETIME = timedelta(days=60)  # around two months
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + join(PROJECT_PATH, 'memorizer.db')
+SQLALCHEMY_DATABASE_URI = config('MEMORIZER_DB', 'sqlite:///' + join(PROJECT_PATH, 'memorizer.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+GOOGLE_ANALYTICS = config('MEMORIZER_GOOGLE_ANALYTICS', None)
 
 # Cache
 CACHE_TIME = 60 * 60 * 2  # 2 hours, pretty arbitrary
-CACHE_TYPE = 'simple'
+CACHE_TYPE = config('MEMORIZER_CACHE_TYPE', 'simple')
 # If redis is used
-CACHE_REDIS_URL = 'redis://localhost:6379'
-
-try:
-    from memorizer.localconfig import *  # NOQA
-except ImportError:
-    pass
+CACHE_REDIS_URL = config('MEMORIZER_REDIS_URL', 'redis://localhost:6379')
